5533b815ebcb217de103dd82f5b9a534661987e9
Christian Seip <christian.seip@ravitar.com>
2024-06-27 15:07:27 +0200

    Update: prevent login for authenticated users

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * Controller managing security-related actions such as login and logout.
+ */
 class SecurityController extends AbstractController
 {
+    /**
+     * Renders the login form or redirects to the homepage if the user is already authenticated.
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_index');
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('user/login/login.html.twig', [
@@ -20,9 +33,14 @@ class SecurityController extends AbstractController
         ]);
     }
 
+    /**
+     * This method can be left blank. It will be intercepted by the logout key configured in your firewall.
+     *
+     * @throws LogicException
+     */
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
